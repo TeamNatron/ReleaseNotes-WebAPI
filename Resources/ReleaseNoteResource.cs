04c3b4867c6764e4335f851f91e297984db9cd19
Lars <larsoyvind28@gmail.com>
2020-02-28 09:57:18 +0100

    fix ReleaseNoteReseource

--- Resources/ReleaseNoteResource.cs
+++ Resources/ReleaseNoteResource.cs
@@ -8,11 +8,12 @@ namespace ReleaseNotes_WebAPI.Resources
         public int Id { get; set; }
         public string Title { get; set; }
         public string Ingress { get; set; }
-        public string DetailedView { get; set; }
+        public string Description { get; set; }
         public string AuthorName { get; set; }
         public string AuthorEmail { get; set; }
         public string WorkItemDescriptionHtml { get; set; }
         public string WorkItemTitle { get; set; }
+        public int WorkItemId { get; set; }
         public DateTime ClosedDate { get; set; }
         public bool IsPublic { get; set; }
     }
