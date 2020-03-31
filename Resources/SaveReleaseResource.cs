34a79273ee473646014fc0093502a3638689e2f9
Markus Randa <randamarkus.95@gmail.com>
2020-03-31 12:48:11 +0200

    Post Releases will now automatically create any ReleaseNotes specified in the body of the request.

--- Resources/SaveReleaseResource.cs
+++ Resources/SaveReleaseResource.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using ReleaseNotes_WebAPI.Domain.Models;
 
 namespace ReleaseNotes_WebAPI.Resources
 {
     public class SaveReleaseResource
     {
+        [Required]
         public int ProductVersionId { get; set; }
         
         public string Title { get; set; }
@@ -12,5 +14,7 @@ namespace ReleaseNotes_WebAPI.Resources
         public bool IsPublic { get; set; }
         
         public IEnumerable<int> ReleaseNotesId { get; set; }
+        
+        public IEnumerable<ReleaseNote> ReleaseNotes { get; set; }
     }
 }
