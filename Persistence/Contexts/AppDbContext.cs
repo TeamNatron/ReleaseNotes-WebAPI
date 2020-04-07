156cb555ec11d60da4a3bcd998c46d6387182b8a
Markus Randa <randamarkus.95@gmail.com>
2020-04-07 16:21:34 +0200

    Implemented the new data authorization system where claims from user accessing data will be used to determine whether the user should get access to all entities or in that specific table or not. At the moment this is implemented for Release, ProductVersion and ReleaseNote.

--- Persistence/Contexts/AppDbContext.cs
+++ Persistence/Contexts/AppDbContext.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using ReleaseNotes_WebAPI.Domain.Models;
 using ReleaseNotes_WebAPI.Domain.Models.Auth;
+using ReleaseNotes_WebAPI.Security;
 
 namespace ReleaseNotes_WebAPI.Persistence.Contexts
 {
     public class AppDbContext : DbContext
     {
+        /**
+         * Truth table for query filter 
+         *
+         * 1 0 == 1 -> Entity is public, and user is not admin
+         * 0 1 == 1 -> Entity is not public, and user is admin
+         * 1 1 == 1 -> Entity is public, and user is admin
+         * 0 0 == 0 -> Entity is not public, and user is not admin
+         * 
+        */
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Article> Articles { get; set; }
@@ -16,9 +28,18 @@ namespace ReleaseNotes_WebAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<ReleaseReleaseNote> ReleaseReleaseNotes { get; set; }
         public DbSet<AzureInformation> AzureInformations { get; set; }
+        private bool UserIsAdmin { get; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor accessor) : base(options)
         {
+            // By default no user is admin
+            UserIsAdmin = false;
+
+            // If the context is setting up for the first time. Don't run
+            if (accessor.HttpContext != null)
+            {
+                UserIsAdmin = ClaimsUtil.CheckIfUserIsAdmin(accessor);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -33,6 +54,17 @@ namespace ReleaseNotes_WebAPI.Persistence.Contexts
             builder.Entity<Release>().Property(r => r.Date).HasDefaultValue(DateTime.UtcNow);
             builder.Entity<ReleaseReleaseNote>().HasKey(
                 rrn => new {rrn.ReleaseId, rrn.ReleaseNoteId});
+            
+            // All entities that require extra security for isPublic
+            builder.Entity<Release>().HasQueryFilter(
+                r => r.IsPublic && !UserIsAdmin || !r.IsPublic && UserIsAdmin ||
+                     r.IsPublic && UserIsAdmin);
+            builder.Entity<ProductVersion>().HasQueryFilter(
+                pr => pr.IsPublic && !UserIsAdmin || !pr.IsPublic && UserIsAdmin ||
+                     pr.IsPublic && UserIsAdmin);
+            builder.Entity<ReleaseNote>().HasQueryFilter(
+                rn => rn.IsPublic && !UserIsAdmin || !rn.IsPublic && UserIsAdmin ||
+                     rn.IsPublic && UserIsAdmin);
         }
     }
 }
