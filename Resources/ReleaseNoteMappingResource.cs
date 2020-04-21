f6f264fbec91b3a4e2222abc9472e6a5672ddb0b
Lars <larsoyvind28@gmail.com>
2020-04-21 18:36:05 +0200

    remove id from RNSMappingResource

--- Resources/ReleaseNoteMappingResource.cs
+++ Resources/ReleaseNoteMappingResource.cs
@@ -4,7 +4,6 @@ namespace ReleaseNotes_WebAPI.Resources
 {
     public class ReleaseNoteMappingResource
     {
-        public int Id { get; set; }
         public string AzureDevOpsField { get; set; }
         public string MappableField { get; set; }
         public string MappableType { get; set; }
