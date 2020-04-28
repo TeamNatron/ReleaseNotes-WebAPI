59882c24a4d5295238a29ed4eeb8e27d6a2b2a97
Lars <larsoyvind28@gmail.com>
2020-04-28 14:09:03 +0200

    add datatype field to mappable

--- Domain/Models/MappableField.cs
+++ Domain/Models/MappableField.cs
@@ -5,5 +5,8 @@ namespace ReleaseNotes_WebAPI.Domain.Models
         public int Id { get; set; }
         
         public string Name { get; set; }
+        public string DataType { get; set; }
+        
     }
+    
 }
